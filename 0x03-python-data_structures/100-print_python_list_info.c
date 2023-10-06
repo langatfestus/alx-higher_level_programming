26f0333d63267b9e5d867a03840afe80192659ba
langatfestus <langatfestus94@gmail.com>
2023-10-06 22:55:59 +0300

    corrections

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,53 +1,20 @@
-#include <stdio.h>
 #include <Python.h>
+#include <object.h>
+#include <listobject.h>
 
 /**
-<<<<<<< HEAD
-* print_python_list_info - prints python list info
-*
-* @p: PyObject
-* Return: no return
-*/
+ * print_python_list_info - prints some basic info about pytohn list
+ * @p: python object
+ **/
+
 void print_python_list_info(PyObject *p)
 {
-  long int size, i;
-  PyListObject *list;
-  PyObject *item;
+  long int size = PyList_Size(p);
+  int i;
+  PyListObject *obj = (PyListObject *)p;
 
-  size = Py_SIZE(p);
-  printf("[*] Size of the Python List = %ld\n", size);
-
-  list = (PyListObject *)p;
-  printf("[*] Allocated = %ld\n", list->allocated);
-
+  printf("[*] Size of the Python List = %li\n", size);
+  printf("[*] Allocated = %li\n", obj->allocated);
   for (i = 0; i < size; i++)
-    {
-      item = PyList_GetItem(p, i);
-      printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-    }
-  =======
-     * print_python_list_info - prints info about a python list
-       * @p: PyObject
-     *
-    *Return: void
-     */
-
-    void print_python_list_info(PyObject *p)
-  {
-   long int size, i;
-   PyListObject *list;
-   PyObject *item;
-
-   size = Py_SIZE(p);
-   printf("[*] Size of the Python List = %ld\n", size);
-
-   list = (PyListObject *)p;
-   printf("[*] Allocated = %ld\n", list->allocated);
-
-   for (i = 0; i < size; i++)
-     {
-       item = PyList_GetItem(p, i);
-       printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
-     }
-   >>>>>>> a75d310d9740983165d2e275ef686696e6fcae3e
-  }
+    printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+}
